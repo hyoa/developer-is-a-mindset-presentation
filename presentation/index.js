b88946650333f73737c7f105bd240e9570598e99
Jules Matsounga <jules.matsounga@link-value.fr>
2019-07-10 08:28:28 +0200

    feat: change some details

--- presentation/index.js
+++ presentation/index.js
@@ -23,10 +23,9 @@ import createTheme from 'spectacle/lib/themes/default';
 const images = {
   formidagon: require('../assets/formidable-logo.svg'),
   goodWork: require('../assets/good-work.gif'),
-  stereotype1: require('../assets/stereotype.png'),
+  stereotype: require('../assets/stereotype.jpg'),
   stereotype2: require('../assets/stereotype2.gif'),
-  stereotype3: require('../assets/stereotype3.gif'),
-  stereotype4: require('../assets/stereotype4.jpg'),
+  stereotype4: require('../assets/stereotype4.png'),
   real1: require('../assets/real1.jpg'),
   real2: require('../assets/real2.png'),
   real3: require('../assets/real3.jpg'),
@@ -34,7 +33,8 @@ const images = {
   bg2: require('../assets/bg2.jpg'),
   bg3: require('../assets/bg3.jpg'),
   bgLast: require('../assets/bg_last.jpg'),
-  bgThanks: require('../assets/bg_thanks.jpg')
+  bgThanks: require('../assets/bg_thanks.jpg'),
+  bgReality: require('../assets/bg_reality.jpg')
 };
 
 // Require CSS
@@ -45,7 +45,8 @@ const theme = createTheme(
     primary: 'white',
     secondary: '#1F2022',
     tertiary: '#03A9FC',
-    quaternary: '#CECECE'
+    quaternary: '#CECECE',
+    dark: "#072529"
   },
   {
     primary: 'Montserrat',
@@ -64,7 +65,7 @@ export default class Presentation extends React.Component {
             Developer is not only a job, it's a mindset
           </Heading>
         </Slide>
-        <Slide bgColor="black" textColor="primary" transition={['fade']}>
+        <Slide bgColor="dark" textColor="primary" transition={['fade']}>
           <Heading size={6} textColor="primary">
             Les développeurs vus par le reste du monde
           </Heading>
@@ -89,6 +90,11 @@ export default class Presentation extends React.Component {
                 "Mais je comprends pas. Il est pas fini ton site depuis le temps ? Tu fais quoi ?"
               </ListItem>
             </Appear>
+            <Appear>
+              <ListItem textSize="30" style={{ marginTop: '1rem' }}>
+                "Ca tombe bien, j'ai un problème avec mon ordinateur..."
+              </ListItem>
+            </Appear>
           </List>
         </Slide>
         <Slide bgImage={images.bg2} bgDarken={0.5} transition={['fade']}>
@@ -96,16 +102,19 @@ export default class Presentation extends React.Component {
             Un développeur c'est quoi ?
           </Heading>
         </Slide>
-        <Slide bgColor="black" transition={['fade']}>
+        <Slide bgColor="dark" transition={['fade']}>
           <Image src={images.stereotype2} />
         </Slide>
-        <Slide bgColor="black" transition={['fade']}>
+        <Slide bgColor="dark" transition={['fade']}>
           <Image src={images.stereotype4} />
         </Slide>
-        <Slide bgColor="black" transition={['fade']}>
+        <Slide bgColor="dark" transition={['fade']}>
+          <Image src={images.stereotype} width={500} />
+        </Slide>
+        <Slide bgColor="dark" transition={['fade']}>
           <Image src="https://media.giphy.com/media/s239QJIh56sRW/source.gif" />
         </Slide>
-        <Slide bgColor="black" transition={['fade']}>
+        <Slide bgColor="dark" transition={['fade']}>
           <Heading textColor="primary">Les raisons sont multiples</Heading>
           <List textAlign="center" textColor="primary">
             <Appear>
@@ -119,19 +128,19 @@ export default class Presentation extends React.Component {
             </Appear>
           </List>
         </Slide>
-        <Slide bgColor="black" transition={['fade']}>
+        <Slide bgImage={images.bgReality} bgDarken={0.5} transition={['fade']}>
           <Heading textColor="white">Mais dans la réalité ?</Heading>
         </Slide>
-        <Slide bgColor="black" transition={['fade']}>
+        <Slide bgColor="dark" transition={['fade']}>
           <Image src={images.real1} width={900}/>
         </Slide>
-        <Slide bgColor="black" transition={['fade']}>
+        <Slide bgColor="dark" transition={['fade']}>
           <Image src={images.real2} width={900}/>
         </Slide>
-        <Slide bgColor="black" transition={['fade']}>
+        <Slide bgColor="dark" transition={['fade']}>
           <Image src={images.real3} />
         </Slide>
-        <Slide bgColor="black" transition={['fade']}>
+        <Slide bgColor="dark" transition={['fade']}>
           <Heading textColor="primary">Developpeur quelque chose</Heading>
           <Appear>
             <List textColor="white">
@@ -145,20 +154,23 @@ export default class Presentation extends React.Component {
             </List>
           </Appear>
         </Slide>
-        <Slide bgColor="black" transition={['fade']}>
+        <Slide bgColor="dark" transition={['fade']}>
           <Heading textSize={34} textColor="primary">
             Un mot pour les gouverner tous
           </Heading>
           <Appear>
-            <Text textColor="primary" textSize={20}>
-              Le développeur informatique est le pro des langages informatiques, tels que C++ ou Java ! Responsable de la programmation, c'est-à-dire de la production de lignes de code, il rédige et suit un cahier des charges précisant les spécificités techniques à suivre pour créer le programme. Afin de concevoir des programmes informatiques « sur mesure », il participe en amont à l'analyse des besoins des utilisateurs, puis à la phase d'essai. En aval, il adapte le logiciel à la demande du client en apportant les retouches nécessaires. Le dévelop-peur prend en charge la formation des utilisateurs de l'application et peut même rédiger un guide d'utilisateur. Par la suite, il intervient pour effectuer la maintenance ou faire évoluer les programmes. Grâce aux progiciels (des logiciels standards de programmation prêts à être utilisés), il passe moins de temps à écrire les programmes, si bien que son activité évolue vers plus d'analyse que de programmation.
-            </Text>
+            <BlockQuote textColor="primary">
+              <Quote textSize={20}>
+                Le développeur informatique est le pro des langages informatiques, tels que C++ ou Java ! Responsable de la programmation, c'est-à-dire de la production de lignes de code, il rédige et suit un cahier des charges précisant les spécificités techniques à suivre pour créer le programme. Afin de concevoir des programmes informatiques « sur mesure », il participe en amont à l'analyse des besoins des utilisateurs, puis à la phase d'essai. En aval, il adapte le logiciel à la demande du client en apportant les retouches nécessaires. Le dévelop-peur prend en charge la formation des utilisateurs de l'application et peut même rédiger un guide d'utilisateur. Par la suite, il intervient pour effectuer la maintenance ou faire évoluer les programmes. Grâce aux progiciels (des logiciels standards de programmation prêts à être utilisés), il passe moins de temps à écrire les programmes, si bien que son activité évolue vers plus d'analyse que de programmation.
+              </Quote>
+              <Cite>Onisep</Cite>
+            </BlockQuote>
           </Appear>
         </Slide>
         <Slide bgImage={images.bg3} bgDarken={0.5} transition={['fade']}>
           <Heading textColor="primary">Et c'est tout ?</Heading>
         </Slide>
-        <Slide bgColor="black" transition={['fade']}>
+        <Slide bgColor="dark" transition={['fade']}>
           <Heading textColor="primary">Compétences techniques</Heading>
           <Appear>
             <List textColor="primary">
@@ -170,7 +182,7 @@ export default class Presentation extends React.Component {
             </List>
           </Appear>
         </Slide>
-        <Slide bgColor="black" transition={['fade']}>
+        <Slide bgColor="dark" transition={['fade']}>
           <Heading textColor="primary">Soft skills</Heading>
           <List textColor="primary">
             <Appear>
@@ -203,7 +215,7 @@ export default class Presentation extends React.Component {
           <Heading textColor="primary">Developer is a mindset</Heading>
         </Slide>
         <Slide bgImage={images.bgThanks} bgDarken={0.4} transition={['fade']}>
-          <Heading textColor="primary">Thanks</Heading>
+          <Heading textColor="primary">Merci à tous !</Heading>
         </Slide>
       </Deck>
     );
